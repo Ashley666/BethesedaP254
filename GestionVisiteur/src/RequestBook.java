b3a7e40078bc832535949de9c0979115269b9132
Ashley666 <kenphiliboy@gmail.com>
2017-01-22 20:02:51 -0500

    ficher de base et interface graphique presque achever

--- GestionVisiteur/src/RequestBook.java
+++ GestionVisiteur/src/RequestBook.java
@@ -8,5 +8,9 @@ public class RequestBook {
 	{
 		System.out.println("Initialisation du livre de requete");
 	}
+	
+	File ficher = new File("test.xml");
+	
+	//StringBuffer string = new StringBuffer(ficher);
 
 }
